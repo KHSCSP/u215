6bd72689b9265940938068e6752c2320e55c786b
heyscottn <heyscottn@gmail.com>
2024-03-07 13:47:09 -0600

    mm

---
+++ debugger_intro.py
@@ -0,0 +1,34 @@
+# this function has no errors
+# we'll use this to become familiar with the debugger tool
+def prob1():
+    num1 = 5
+    num2 = 10
+    ans = num1+num2
+    ans = ans + 3
+    ans = ans/2
+    print(ans)
+
+# this function does have an error
+# let's analyze with the debugger
+def prob2():
+    num1 = 0
+    num2 = 5
+    num3 = 10
+    ans = num2+num3
+    ans = ans/num1
+    print(ans)
+
+
+# this function also has an error
+def prob3():
+    my_list = []
+    num = 5
+    for i in range(num):
+        if my_list[i] > 0:
+            print("positive")
+        else:
+            print("negative")    
+
+# prob1()
+prob2()
+# prob3()
