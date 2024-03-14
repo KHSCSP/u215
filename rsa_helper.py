4c311d2e87db55fb65a95651003d3b92293b9f79
heyscottn <heyscottn@gmail.com>
2024-03-14 12:14:30 -0500

    mm

--- rsa_helper.py
+++ rsa_helper.py
@@ -8,12 +8,12 @@ print("is this a digit?", stuff.isdigit())
 # --- combine this with input ---
 ans = ''
 while not ans.isalpha():
-  ans = input("\ntype letters only:")
+    ans = input("\ntype letters only:")
 
 
 
 # --- what if too short?? ---
 ans = ''
 while len(ans) < 8 or not ans.isalpha():
-  ans = input("\ntype at least 8 letters: ")
+    ans = input("\ntype at least 8 letters: ")
   
